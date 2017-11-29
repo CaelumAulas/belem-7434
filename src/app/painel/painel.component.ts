52a162ba9c1a0fda3796a0617cf0ca24ddd6548f
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-11-29 09:28:59 -0200

    Feito até o 5.3

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-painel',
-  templateUrl: './painel.component.html'
+  templateUrl: './painel.component.html',
+  styleUrls: ['./painel.component.css']
 })
 export class PainelComponent {
   @Input() titulo: String = '';
